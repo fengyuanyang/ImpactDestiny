a0d50806d528db5f4dbd1ee0faa8596d6f3a1790
OwenYang <coolsealtw@hotmail.com>
2021-03-07 17:44:29 +0800

    add defult sproing boot setting

---
+++ src/main/java/com/ordestiny/impact/ImpactApplication.java
@@ -0,0 +1,13 @@
+package com.ordestiny.impact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpactApplication.class, args);
+    }
+
+}
